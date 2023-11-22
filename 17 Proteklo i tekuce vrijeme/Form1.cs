e12284a3a42f2cdb8084044d57fb962f93d84a82
JustGabi <113007885+ClashMania@users.noreply.github.com>
2023-11-22 08:22:43 +0100

    Add files via upload

---
+++ 17 Proteklo i tekuce vrijeme/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17_Proteklo_i_tekuce_vrijeme
+{
+    public partial class Form1 : Form
+    {
+
+        DateTime pocetno = DateTime.Now;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            toolStripStatusLabel2.Text = "Trenutno: " + DateTime.Now.ToLongTimeString();
+            TimeSpan proteklo = new TimeSpan();
+            proteklo = DateTime.Now - pocetno;
+            this.toolStripStatusLabel1.Text = "Proteklo: " +
+            proteklo.Hours.ToString() + ":" + proteklo.Minutes.ToString() + ":" + proteklo.Seconds.ToString();
+        }
+    }
+}
